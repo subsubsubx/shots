3e4964a227e2de0fa295a1c7026b35292a84c322
subsubsubx <you@example.com>
2024-03-20 21:54:40 +0300

    add arrays tostring

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.math.BigInteger;
+import java.util.Arrays;
 import java.util.EnumSet;
 import java.util.Set;
 
@@ -8,6 +9,7 @@ public class Main {
 
     public static void main(String[] args) {
         Set<Grade> set = EnumSet.of(Grade.B, Grade.C, Grade.D);
+        System.out.println(Arrays.toString(set.toArray()));
     }
 
 }
