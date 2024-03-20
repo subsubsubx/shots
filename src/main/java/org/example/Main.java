37fdb47e8fee1e47d1fc995e777f02dd0816dab1
subsubsubx <you@example.com>
2024-03-20 22:09:37 +0300

    feat123

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,6 +10,7 @@ public class Main {
     public static void main(String[] args) {
         Set<Grade> set = EnumSet.of(Grade.B, Grade.C, Grade.D);
         System.out.println(Arrays.toString(set.toArray()));
+        //feature qwe123
     }
 
 }
