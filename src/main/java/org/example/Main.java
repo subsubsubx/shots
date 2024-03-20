4185c4ed20ac65235bc44f728e4803e315e4d511
subsubsubx <you@example.com>
2024-03-20 22:18:43 +0300

    kal

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
         Set<Grade> set = EnumSet.of(Grade.B, Grade.C, Grade.D);
         System.out.println(Arrays.toString(set.toArray()));
-        //resolve conf
+        //r987
     }
 
 }
